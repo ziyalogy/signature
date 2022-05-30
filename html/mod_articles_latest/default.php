322e5420b92d6112ceb347882a06cfa8f11a3882
ziyalogy <ziyalogy@gmail.com>
2022-05-30 17:56:34 +0300

    Team module image fix

--- html/mod_articles_latest/default.php
+++ html/mod_articles_latest/default.php
@@ -3,29 +3,28 @@
  * ------------------------------------------------------------------------
  * ja_justitia_tpl
  * ------------------------------------------------------------------------
- * Copyright (C) 2004-2011 Buildal Systems (U) Co., Ltd. All Rights Reserved.
+ * Copyright (C) 2004-2011 J.O.O.M Solutions Co., Ltd. All Rights Reserved.
  * @license - Copyrighted Commercial Software
- * Author: Buildal Systems (U) Co., Ltd
- * Websites:  http://www.buildal.ug -  http://www.buildal.ug
+ * Author: J.O.O.M Solutions Co., Ltd
+ * Websites:  http://www.joomlart.com -  http://www.joomlancers.com
  * This file may not be redistributed in whole or significant part.
  * ------------------------------------------------------------------------
- */
+*/
 
-defined('_JEXEC') or die(); ?>
-<ul class="latestnews<?php echo $params->get('moduleclass_sfx'); ?>">
-<?php foreach ($list as $item): ?>
+defined('_JEXEC') or die;
+?>
+<ul class="latestnews<?php echo $params->get ('moduleclass_sfx') ?>">
+<?php foreach ($list as $item) :  ?>
 	<li class="clearfix" >
 		<div class="pic-article">
-			<?php if (json_decode($item->images)->image_intro): ?>
+			<?php if(json_decode($item->images)->image_intro) :?>
 				<img src="<?php echo json_decode($item->images)->image_intro; ?>" alt="" />
-			<?php endif; ?>
+			<?php endif ;?>
 		</div>
 
 		<div class="info-article">
 			<div class="category-name">
-				<a href="<?php echo JRoute::_(
-        ContentHelperRoute::getCategoryRoute($item->catid)
-    ); ?>" ><?php echo $item->category_title; ?></a>
+				<a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($item->catid)); ?>" ><?php echo $item->category_title?></a>
 			</div>
 			<div class="title-article">
 				<h6>
